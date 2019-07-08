026fbbdc63ddd039d0d5c4ad9d3e62473900b055
brymz <z.t.brym@gmail.com>
2019-07-08 16:51:21 -0400

    harvestanalysis.R: format raster figure; add mass/stem bar chart per species

--- harvestanalysis.R
+++ harvestanalysis.R
@@ -6,7 +6,7 @@ library(agricolae)
 library(raster)
 library(RColorBrewer)
 
-matrix_plot <- function(data_for_matrix){
+matrix_plot <- function(data_for_matrix, title){
   plotid_matrix <- matrix(data = c(1:144), nrow=24, ncol=6, byrow=TRUE)
   biomass_std_matrix <- matrix(nrow=24, ncol=6)
   biomass_std_names <- matrix(nrow=24, ncol=6)
@@ -19,28 +19,29 @@ matrix_plot <- function(data_for_matrix){
     }
   }
   
-  par(mfrow=c(2,2))
-  plot(raster(biomass_std_matrix[1:6,]), axes=FALSE, 
+  par(mfrow=c(2,2), mar=c(1,0,1,0))
+  plot(raster(biomass_std_matrix[1:6,]), axes=FALSE, box=FALSE, legend=FALSE,
        zlim=c(min(data_for_matrix$sum_biomass_std), max(data_for_matrix$sum_biomass_std)),
        breaks= c(3, 2, 1, 0, -1, -2, -3), col= brewer.pal(7, "RdBu"))
   text(x=rep(c(0.1, 0.3, 0.5, 0.65, 0.8, 1),6), 
        y=rep(c(1, 5/6, 4/6, 3/6, 2/6, 1/6),each=6),
        label=data_for_matrix$CropTrt[1:36], adj=c(1,1))
-  plot(raster(biomass_std_matrix[7:12,]), axes=FALSE, 
+  plot(raster(biomass_std_matrix[7:12,]), axes=FALSE, box=FALSE, legend=FALSE,
        zlim=c(min(data_for_matrix$sum_biomass_std), max(data_for_matrix$sum_biomass_std)),
        breaks= c(3, 2, 1, 0, -1, -2, -3), col= brewer.pal(7, "RdBu"))
   text(x=rep(c(0.1, 0.3, 0.5, 0.65, 0.8, 1),6), 
        y=rep(c(1, 5/6, 4/6, 3/6, 2/6, 1/6),each=6),
        label=data_for_matrix$CropTrt[36:72], adj=c(1,1))
-  plot(raster(biomass_std_matrix[13:18,]), axes=FALSE, 
+  plot(raster(biomass_std_matrix[13:18,]), axes=FALSE, box=FALSE, legend=FALSE, 
        zlim=c(min(data_for_matrix$sum_biomass_std), max(data_for_matrix$sum_biomass_std)),
        breaks= c(3, 2, 1, 0, -1, -2, -3), col= brewer.pal(7, "RdBu"))
   text(x=rep(c(0.1, 0.3, 0.5, 0.65, 0.8, 1),6), 
        y=rep(c(1, 5/6, 4/6, 3/6, 2/6, 1/6),each=6),
        label=data_for_matrix$CropTrt[73:108], adj=c(1,1))
-  plot(raster(biomass_std_matrix[19:24,]), axes=TRUE, 
+  plot(raster(biomass_std_matrix[19:24,]), axes=FALSE,  box=FALSE,
        zlim=c(min(data_for_matrix$sum_biomass_std), max(data_for_matrix$sum_biomass_std)),
-       breaks= c(3, 2, 1, 0, -1, -2, -3), col= brewer.pal(7, "RdBu"))
+       breaks= c(3, 2, 1, 0, -1, -2, -3), col= brewer.pal(7, "RdBu"),
+       legend.width=4,  legend.args=list(text=title, side=2, cex=2))
   text(x=rep(c(0.1, 0.3, 0.5, 0.65, 0.8, 1),6), 
        y=rep(c(1, 5/6, 4/6, 3/6, 2/6, 1/6),each=6),
        label=data_for_matrix$CropTrt[109:144], adj=c(1,1))
@@ -61,7 +62,9 @@ seeding_rate_table <- data.frame(CropTrt = c("SH", "SS", "VB",
 biomass_data <- read_csv("data/harvest-012019-TREC.csv") %>% 
   separate(PlotName, c("Location_CropTrt", "Row"), sep = "-") %>%
   separate(Location_CropTrt, c("Location", "CropTrt"), sep = "_", remove=FALSE) %>%
-  mutate(LeavesStems_tha = LeavesStems_g_1m2*0.01) %>%
+  mutate(Location = factor(Location, levels = c("VF", "FO", "OC", "HH")),
+         CropTrt = factor(CropTrt, levels = c("SH", "SS", "VB", "SHSS", "SHVB", "SSVB")),
+         LeavesStems_tha = LeavesStems_g_1m2*0.01) %>%
   left_join(seeding_rate_table)
 
 total_biomass <- biomass_data %>%
@@ -117,7 +120,7 @@ combined_biomass_std <- combined_biomass %>%
            global_sd_combined_biomass)
 
 png(file="output/combined_std.png", width= 1700, height=1100)
-matrix_plot(combined_biomass_std)
+matrix_plot(combined_biomass_std, "+/- Global SD")
 dev.off()
 
 trt_avg_combined_biomass <- combined_biomass %>% 
@@ -129,7 +132,7 @@ combined_biomass_std_trt <- combined_biomass %>%
   mutate(sum_biomass_std = treatment_standardization(CropTrt, trt_avg_combined_biomass, sum_LeavesStems_tha))
 
 png(file="output/combined_std_trt.png", width= 1700, height=1100)
-matrix_plot(combined_biomass_std_trt)
+matrix_plot(combined_biomass_std_trt, "+/- Treatment SD")
 dev.off()
 
 ## ANOVA for average stem biomass per species.
@@ -147,7 +150,7 @@ for(s in species){
     filter(CropSp == s)
   
   test <- aov(avg_LeavesStems_tha ~  CropTrt + Location_CropTrt, data=data_for_analysis)
-  print(paste(s, "- Fresh Mass"))
+  print(paste(s, "- Fresh Mass [t/ha]"))
   print(summary(test))
   print(HSD.test(test, "CropTrt")$groups)
   print(HSD.test(test, "Location_CropTrt")$groups)
@@ -164,19 +167,38 @@ for(s in species){
   print(HSD.test(test, "CropTrt")$groups)
   print(HSD.test(test, "Location_CropTrt")$groups)
   
-  test <- aov(avg_LeavesStems_tha/avg_StemCount ~  CropTrt + Location_CropTrt, data=data_for_analysis)
-  print(paste(s, "- Fresh Mass / Stem Count"))
+  test <- aov(avg_LeavesStems_tha/(0.01*avg_StemCount) ~  CropTrt + Location_CropTrt, data=data_for_analysis)
+  print(paste(s, "- Fresh Mass / Stem Count [g/ind]"))
   print(summary(test))
   print(HSD.test(test, "CropTrt")$groups)
   print(HSD.test(test, "Location_CropTrt")$groups)
   
-  test <- aov(avg_LeavesStems_tha/avg_StemCount ~  Location + CropTrt + Location*CropTrt, data=data_for_analysis)
-  print(paste(s, "- Fresh Mass / Stem Count"))
+  test <- aov(avg_LeavesStems_tha/(0.01*avg_StemCount) ~  Location + CropTrt + Location*CropTrt, data=data_for_analysis)
+  print(paste(s, "- Fresh Mass / Stem Count [g/ind]"))
   print(summary(test))
   print(HSD.test(test, "Location")$groups)
   print(HSD.test(test, "CropTrt")$groups)
+  
 }
 sink()
+
+for(s in species){
+  data_for_figure <- total_biomass %>% 
+    ungroup() %>%
+    filter(CropSp == s) %>%
+    mutate(CropTrt = factor(CropTrt, levels = str_subset(treatments, s))) %>% 
+    group_by(Location, CropTrt) %>%
+    summarize(avg_stem_mass = mean(avg_LeavesStems_tha/(0.01*avg_StemCount), na.rm=TRUE),
+              se_stem_mass = sd(avg_LeavesStems_tha/(0.01*avg_StemCount*sqrt(n())), na.rm=TRUE))
+
+  ggplot(data_for_figure, aes(x=factor(CropTrt), y=avg_stem_mass)) +
+    geom_bar(stat="identity") +
+    geom_errorbar(aes(ymin = avg_stem_mass-se_stem_mass, ymax = avg_stem_mass+se_stem_mass), width=0.2) +
+    facet_grid(.~Location) +
+    labs(x="Crop Mix", y="Fresh Mass / Individual [g]", title = paste0(s)) +
+    theme_bw(base_size = 24, base_family = "Helvetica")
+  ggsave(paste0("output/Stem_mass_", s, ".png"))
+}
 
 ## Land Equivalent Ratio
 data_for_LER <- total_biomass %>%
