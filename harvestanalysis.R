42f1da5fbf15fb0feac4d487a2328d1ba058bf91
brymz <z.t.brym@gmail.com>
2019-10-31 14:21:22 -0400

    merge harvestanalysis.R

--- harvestanalysis.R
+++ harvestanalysis.R
@@ -112,7 +112,9 @@ moisture_species <- moisture_content %>%
   summarize(avg_MC = mean(AGB_MC))
 
 species <- c("SH", "SS", "VB")
-treatments <- c("SH", "SS", "VB", "SHSS", "SHVB", "SSVB")
+treatments <- data.frame(treatments = c("SH", "SS", "VB", "SHSS", "SHVB", "SSVB"),
+                         color_set = c("#A93226", "#D4AC0D", "#85C1E9", "#AF601A", "#8E44AD", "#196F3D"))
+
 
 ## Species biomass per site
 
@@ -130,7 +132,7 @@ print(HSD.test(test, "CropTrt")$groups)
 print(HSD.test(test, "Location_CropTrt")$groups)
 
 ggplot(monos_summary, aes(x=Location, y=Avg_LeavesStems_tha)) +
-  geom_bar(stat="identity") +
+  geom_bar(stat="identity", aes(fill=Location)) +
   geom_errorbar(aes(ymin = Avg_LeavesStems_tha-CI_LeavesStems_tha, 
                     ymax = Avg_LeavesStems_tha+CI_LeavesStems_tha), width=0.2) +
   facet_grid(.~CropSp) +
@@ -234,11 +236,14 @@ for(s in species){
   data_for_figure <- total_biomass %>% 
     ungroup() %>%
     filter(CropSp == s) %>%
-    mutate(CropTrt = factor(CropTrt, levels = str_subset(treatments, s))) %>% 
+    mutate(CropTrt = factor(CropTrt, levels = str_subset(treatments$treatments, s))) %>%
     group_by(Location, CropTrt) %>%
     summarize(avg_stem_mass = mean(avg_LeavesStems_tha/(0.01*avg_StemCount), na.rm=TRUE),
-              ci_stem_mass = 2*sd(avg_LeavesStems_tha/(0.01*avg_StemCount), na.rm=TRUE)/sqrt(n()))
+              ci_stem_mass = 2*sd(avg_LeavesStems_tha/(0.01*avg_StemCount), na.rm=TRUE)/sqrt(n())) %>% 
+    left_join(treatments, by = c("CropTrt" = "treatments")) %>% 
+    mutate(CropTrt = factor(CropTrt, levels = str_subset(treatments$treatments, s)))
 
+<<<<<<< HEAD
   ggplot(data_for_figure, aes(x=CropTrt, y=avg_stem_mass)) +
     geom_bar(aes(fill=CropTrt), stat="identity") +
     geom_errorbar(aes(ymin = avg_stem_mass-ci_stem_mass, ymax = avg_stem_mass+ci_stem_mass), width=0.2) +
@@ -246,6 +251,16 @@ for(s in species){
     labs(x="Crop Mix", y="Fresh Mass / Individual [g +/- 95% CI]", title = paste0(s)) +
     theme_bw(base_size = 20, base_family = "Helvetica") +
     theme(legend.position = "none")
+=======
+  ggplot(data_for_figure, aes(x=factor(CropTrt), y=avg_stem_mass)) +
+    geom_bar(stat="identity", aes(fill=CropTrt)) +
+    geom_errorbar(aes(ymin = avg_stem_mass-ci_stem_mass, ymax = avg_stem_mass+ci_stem_mass), width=0.2) +
+    facet_grid(.~Location) +
+    labs(x="Crop Mix", y="Fresh Mass / Individual [g +/- 95% CI]", title = paste0(s)) +
+    scale_fill_manual(values=as.character(unique(data_for_figure$color_set))) +
+    theme_bw(base_size = 24, base_family = "Helvetica") +
+    theme(legend.position="none")
+>>>>>>> 78085ca... harvestanalysis.R: color figs
   ggsave(paste0("output/Stem_mass_", s, ".png"))
 }
 
@@ -317,10 +332,11 @@ LER_row_summary <- LER_output_row %>%
   mutate(spp = paste0(sp_1, "-", sp_2))
 
 ggplot(LER_row_summary, aes(x=spp, y=mean_LER)) +
-  geom_bar(stat="identity") +
+  geom_bar(stat="identity", aes(fill=spp)) +
   geom_errorbar(aes(ymin=mean_LER-ci_LER, ymax=mean_LER+ci_LER), width=0.2) +
   facet_grid(.~l) +
   geom_hline(yintercept = 1, linetype="dashed") +
+  scale_fill_manual(values=c("#AF601A", "#8E44AD", "#196F3D")) +
   labs(x="Crop Mix", y="LER [+/- 95% CI]") +
   theme_bw(base_size = 24, base_family = "Helvetica") +
   theme(axis.text.x = element_text(size=14, angle=30))
